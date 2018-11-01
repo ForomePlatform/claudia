280c42b50076feba8bb3a62f7c930a2e0d82b8cf
11235andrew <sunts_andrew@mail.ru>
2018-11-02 00:10:35 +0300

    Redactor + interpretator

---
+++ claudia_interpretator.py
@@ -0,0 +1,341 @@
+import sys
+import json
+import copy
+from lxml import etree
+from mongodb import get
+from mongodb import put
+from mongodb import connect
+from annotators import IsNumericAnnotator,  taxonomy,  RegExpAnnotator
+
+# Apply code to all datasets
+def all_datasets(mongo):
+    datasets=[
+                'cci', 
+                'nets', 
+                'medications'
+                ]
+    for ds in datasets:
+        print('DataSet: ' + ds)
+        all_files(ds,  mongo)
+
+#  Apply JSON-code for all documents in 'dir.list' and all steps of JSON-code.  
+#  It's independent with 'def all_steps'.
+def all_files(dataset,  mongo):
+    chf = []
+    formula = 'CHF'
+    indexes = get("all_indexes", dataset=dataset,  mongo=mongo)
+    code = get("code.cla.json", formula=formula, mongo=mongo)
+    for number_of_card in indexes:
+        print('Card #' + number_of_card)
+        doc_data = create_dict(dataset,  number_of_card,  mongo)
+        all_steps(code, doc_data, dataset,  number_of_card,  mongo,  False)
+        put("annotations",  doc_data['data'],  
+                dataset=dataset,  number_of_card=number_of_card, 
+                formula = formula,  mongo=mongo)
+        if 'ICHF' in doc_data['data']:
+            chf.append(number_of_card)
+    put("calculated_indexes",  chf,  formula=formula,  
+                dataset=dataset,  mongo=mongo)
+
+# Apply function 'annotator' for all document of a patient
+def all_documents(annotator,  doc_data,  old_step,  mongo,  step_id):
+    for document in doc_data['documents']:
+        step = copy.deepcopy(old_step)
+        annotator(document,  step,  mongo,  step_id)
+
+#  Apply function 'annotator' for all sentence of a document.  
+def all_sentences(annotator,  doc_data,  old_step,  mongo,  step_id):
+    for document in doc_data['documents']:
+        for sentence in document['sentences']:
+            step = copy.deepcopy(old_step)
+            annotator(sentence,  step,  mongo,  step_id)
+
+#  Apply function 'annotator' for all chunks of a document. 
+def all_chunks(annotator, doc_data, old_step,  mongo,  step_id):
+    for document in doc_data['documents']:
+        for sentence  in document['sentences']:
+            for chunk in sentence['chunks']:
+                step = copy.deepcopy(old_step)
+                annotator(chunk,  step,  mongo,  step_id)
+
+
+#  Apply 'interpretator' for all steps of JSON-code in 'json_file_name' for a document.
+#  'snap_file' is a snapshot file.  
+def all_steps(code,  doc_data, dataset,  number_of_card,  mongo,  with_snap):
+    for step in code['statements']:
+        claudia_interpretator(doc_data,  dataset,  
+                                        number_of_card, step,  mongo, with_snap,  -1)
+
+
+#  Represent a document to a list 'doc_data' of sentences. Every sentence is a list of chunks.
+#  Every chunk is a dictionary:
+#
+#  text: 'chunk'
+#  data: '{key: value, ... }
+#
+#  Attribute 'data' is empty.  
+def create_dict(dataset,  patient,  mongo):
+    doc_data = {}
+    doc_data['data'] = {}
+    doc_data['documents'] = []
+    doc = {}
+    doc['data'] = {}
+    doc['sentences'] = []
+    #sHTML_Parser = etree.HTMLParser(remove_comments = True)
+    samples = get("doc.html",  dataset=dataset,  
+                                number_of_card=patient,  mongo=mongo)
+    for node in samples:
+        sample = etree.fromstring(node)
+        sentence = {}
+        sentence['data'] = {}
+        sentence['chunks'] = []
+        s = etree.tostring(sample)
+        ss = etree.fromstring(s)
+        for nd_alevel in ss.xpath('/p/span/span'):
+            alevel = nd_alevel.attrib
+            s = etree.tostring(nd_alevel)
+            sss = etree.fromstring(s)
+            for nd in sss.xpath('/span/span/span'):
+                chunk = {}
+                chunk['text'] = nd.text
+                chunk['data'] = {}
+                chunk['data']['__negation'] = alevel['class'][6:]
+                sentence['chunks'].append(chunk)
+        doc['sentences'].append(sentence)
+    doc_data['documents'].append(doc)
+    return doc_data
+
+def apply_tax(chunk,  step,  mongo,  step_id):
+    if step_id != step['source_id'] and step_id != -1:
+        return
+    for tax in step['options']['values']:
+        if tax == 'NUMERIC':
+            dict = IsNumericAnnotator(chunk['text'],  mongo)
+        else:
+            dict = taxonomy(chunk['text'],  tax,  mongo)
+        chunk['data'].update(dict)
+
+def loop(doc_data, dataset, number_of_card,  step,  mongo,  with_snap,  step_id):
+    for new_step in step['options']['action']:
+        if step['set'] == 'entities':
+            all_chunks(one_step,  doc_data,  new_step,  mongo,  step_id)
+        elif step['set'] == 'sentences':
+            all_sentences(one_step,  doc_data,  new_step,  mongo,  step_id)
+        elif step['set'] == 'documents':
+            all_documents(one_step, doc_data,  new_step,  mongo,  step_id)
+        else:
+            print('Unknoun loop set: ' + str(step['set']))
+            sys.exit(0)
+        if with_snap:
+            snapshot(dataset,  number_of_card, doc_data, mongo)
+
+def one_step(data,  step,  mongo,  step_id):
+    if step['action'] == 'detect':
+        if step['name'] == 'if':
+            return statement(data,  step['options'],  mongo,  step_id)
+        elif step['name'] == 'annotate':
+            annotate(data,  step,  step_id)
+        else:
+            print('Unknown name: ' + step['name'])
+            sys.exit(1)
+    elif step['action'] == 'reject':
+        reject(data,  step,  step_id)
+    else:
+        print('Unknown action: ' + step['action'])
+        sys.exit(2)
+
+
+def reject(data,  step,  step_id):
+    if step_id != step['source_id'] and step_id != -1:
+        return
+    data['data']['reject'] = 'reject'
+
+
+#  Conditional operator: if ... then ... .
+def statement(data,  step,  mongo,  step_id):
+    cond = condition(data, step['condition'],  step['args'])
+    if cond:
+        for new_step in step['action_if']:
+            return one_step(data,  new_step,  mongo,  step_id)
+    else:
+        if 'action_else' in step:
+            for new_step in step['action_else']:
+                return one_step(data,  new_step,  mongo,  step_id)
+
+#  Conditional operator 'if'. See 'def statement'.  
+def condition(data,  cond,  args):
+    #print('f: ' + cond['f'] + ' args: ' + str(args))
+    #print('cond: ' + json.dumps(cond,  indent=4))
+    if cond['f'] == 'annotated':
+        return annotated(data,  cond,  args)
+    elif cond['f'] == 'and':
+        return conjunction(data,  cond['a'],  args)
+    elif cond['f'] == 'or':
+        return disjunction(data,  cond['a'],  args)
+    elif cond['f'] == 'not':
+        return negative(data,  cond['a'],  args)
+    elif cond['f'] == 'equals':
+        return equals(data,  cond['a'],  args)
+#    elif cond['f'] == 'annotationData':
+#        return annotationData(data,  cond['a'],  args)
+    elif cond['f'] == 'x':
+        return variable(data,  args)
+#    elif cond['f'] == 'annotationData':
+#        return annotation_data(data, dict)
+#    elif cond['f'] == 'const':
+#        return data['value']
+    else:
+        print('Unknown function: ' + cond['f'])
+        sys.exit(7)
+
+def annotated(data,  cond,  args):
+    #print('data: ' + str(data))
+    n_ann = len(cond['a']) + 1
+    filds = args[1:n_ann]
+    if 'documents' in data:
+        set = 'documents'
+    elif 'sentences' in data:
+        set = 'sentences'
+    elif 'chunks':
+        set = 'chunks'
+    else:
+        set = 'text'
+    if set == 'text':
+        arg = args[0]
+        args.pop(0)
+        if arg['type'] == 'key':
+            if arg['value'] in data['data']:
+                res = True
+            else:
+                res = False
+            for ann in cond['a']:
+                res = condition(data,  ann,  args) and res
+            return res
+        else:
+            print('Unknown type: ' + arg['type'])
+            sys.exit(8)
+    else:
+        for chunk in cond['set']:
+            pass
+
+
+#  'And'
+def conjunction(data,  cond,  args):
+    cond1 = condition(data, cond[0],  args)
+    cond2 = condition(data,  cond[1],  args)
+    return cond1 and cond2
+
+
+#  'Or'
+def disjunction(data,  cond,  args):
+    cond1 = condition(data, cond[0],  args)
+    cond2 = condition(data,  cond[1],  args)
+    return cond1 or cond2
+
+
+#  'Not'
+def negative(data,  cond,  args):
+    return not condition(data,  cond,  args)
+
+
+#  '='
+def equals(data,  cond,  args):
+    a = condition(data,  cond[0],  args)
+    b = condition(data,  cond[1],  args)
+    if a is None or b is None:
+        return False
+    return a == b
+#    if a == b:
+#        return True
+#    else:
+#        return False
+
+
+def variable(data,  args):
+    arg = args[0]
+    args.pop(0)
+    if arg['type'] == 'annotationData':
+        if arg['value'] in data['data']:
+            res = data['data'][arg['value']]
+            return res
+    elif arg['type'] == 'const':
+        res = arg['value']
+        return res
+    else:
+        print('Unknown type: ' + args[0]['type'])
+        print(args[0])
+        sys.exit(9)
+
+
+def annotate(data,  step,  step_id):
+#    elements = ['chunks',  'sentences',  'documents']
+#    for el in elements:
+#        if el in data:
+#            n = 0
+#            for chunk in data[el]:
+#                if step['key'] in chunk:
+#                    n += 1
+#            data['data'][step['key']] = n
+#    if step['key'] not in data['data']:
+    if step_id != step['source_id'] and step_id != -1:
+        return
+    data['data'][step['key']] = step['key']
+    data['data'].update(step['options'])
+
+
+#  Save all results about a document in file 'file_name'. 
+def snapshot(dataset,  number_of_card,  doc_data,  mongo):
+    put("snap.json",  doc_data,  dataset=dataset,  
+                                number_of_card=number_of_card,  mongo=mongo)
+
+
+def setFinalAnnotation(doc_data):
+    pass
+
+
+#  The interpretator of step 'step' of JSON-code appying for a document ('doc-data').  
+#  Snapshots are in 'snap-file'.  
+def claudia_interpretator(doc_data, dataset, number_of_card,  step,  mongo, with_snap,  step_id):
+    if step['action'] == 'for':
+        loop(doc_data, dataset,  number_of_card,  
+                                        step,  mongo, with_snap,  step_id)
+    elif step['action'] == 'detect':
+        if step['name'] == 'lookup':
+            all_chunks(apply_tax,  doc_data,  step,  mongo,  step_id)
+        elif step['name'] == 'setFinalAnnotations':
+            setFinalAnnotation(doc_data)
+        else:
+            print('Unknown operator: ' + step['name'])
+            sys.exit(3)
+    else:
+        print('Unknown action: ' + step['action'])
+        sys.exit(4)
+    return doc_data
+
+
+#  Apply the interpretator for a step 'step_id' of JSON-code in 'code_file_name'
+#  for a document 'doc_file_name'. 'snap_file_name' is a snaphot file.
+#  It's independent with 'def all_files'.
+def next_step(code, dataset, number_of_card,  step_id,  mongo):
+#def next_step(doc_file_name,  code_file_name, snap_file_name, step_id):
+    print('step: ' + str(step_id))
+    if step_id == 0:
+        doc_data = create_dict(dataset, number_of_card,  mongo)
+        snapshot(dataset,  number_of_card,  doc_data,  mongo)
+    doc_data = get("snap.json", dataset=dataset,  
+                                number_of_card=number_of_card,  mongo=mongo)
+    if doc_data is None:
+        doc_data = create_dict(dataset,  number_of_card,  mongo)
+    #print('statements: ' + json.dumps(code['source']))
+    for step in code['statements']:
+        claudia_interpretator(doc_data, dataset, 
+                                                    number_of_card,  step,  mongo,  True,  step_id)
+#            break
+    snapshot(dataset,  number_of_card,  doc_data,  mongo)
+
+
+if __name__ == '__main__':
+    mongo = connect()
+    #all_files(mongo)
+    all_datasets(mongo)
+    print('Ok.')
